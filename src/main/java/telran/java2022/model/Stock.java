41096da38dddc83fd028ccf58ce754199f62950b
GenaTkach <takachgena@gmail.com>
2022-12-22 19:20:14 +0200

    Fine-tuning of CSV Parser

--- src/main/java/telran/java2022/model/Stock.java
+++ src/main/java/telran/java2022/model/Stock.java
@@ -1,14 +1,17 @@
 package telran.java2022.model;
 
+import java.io.Serializable;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @EqualsAndHashCode(of = "date")
@@ -16,13 +19,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
-public class Stock {
+@ToString
+public class Stock implements Serializable {
+    private static final long serialVersionUID = 998443373406764689L;
     @Id
     String date;
-    String price;
-    String open;
-    String high;
-    String low;
-    String vol;
-    String change;
+    String close;
 }
--- src/main/java/telran/java2022/service/Service.java
+++ src/main/java/telran/java2022/service/Service.java
@@ -1,48 +1,53 @@
 package telran.java2022.service;
 
-import java.io.File;
-import java.nio.charset.Charset;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
+import org.springframework.stereotype.Component;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
 import lombok.RequiredArgsConstructor;
 import telran.java2022.dao.Repository;
 import telran.java2022.model.Stock;
 
-@org.springframework.stereotype.Service
 @RequiredArgsConstructor
-public class Service implements CommandLineRunner {
+@Component
+public class Service {
 
+    // Подключаем репозиторий, чтобы был доступ к базе данных
     final Repository repository;
 
-    // Подготовка CSVParser
-    private File file = new File("/Users/Gena/Desktop/Desc/Coding/TELRAN PROJECT/S&P 500 Historical Data.csv");
-    CSVParser parser;
-    Charset charset = Charset.defaultCharset();
-    CSVFormat format = CSVFormat.DEFAULT.withFirstRecordAsHeader();
-
-    @Override
-    public void run(String... args) throws Exception {
-	parser = CSVParser.parse(file, charset, format);
-	for (CSVRecord record : parser) {
-	    System.out.println(record);
-	    record.stream()
-		    .forEach(s -> System.out.println(s));
-	    
-	    Stock stock = new Stock();
-	    stock.setDate(record.get(0));
-	    stock.setPrice(record.get(1));
-	    stock.setOpen(record.get(2));
-	    stock.setHigh(record.get(3));
-	    stock.setLow(record.get(4));
-	    stock.setChange(record.get(6));
-	    if (!repository.existsById(stock.getDate())) {
-		repository.save(stock);
+    @Bean
+    public void CSVMaptoObject() throws FileNotFoundException {
+	// Cоздаем объект, который парсит из CSV -> Stock
+	CsvToBean<Stock> csvToBean = new CsvToBean<>();
+	// Локальный путь к CSV файлу
+	String pathToCsvFile = "/Users/Gena/Desktop/Desc/Coding/TELRAN PROJECT/HistoricalPrices.csv";
+	CSVReader csvReader = new CSVReader(new FileReader(pathToCsvFile));
+	
+	// Парсинг в лист Stocks
+	List<Stock> list = csvToBean.parse(setMapStrategy(), csvReader);
+	for (Stock stock : list) {
+	    // После парсинга создаем из каждой строки листа -> Java объект Stock
+	    Stock stockPerDay = stock;
+	    //Делаем проверку и отсекаем первую строку (заголовки)
+	    if (!stockPerDay.getDate().equalsIgnoreCase("date")) {
+		repository.save(stockPerDay);
 	    }
 	}
-	parser.close();
+    }
+    
+    // Устанавливаем парсинг-стратегию
+    private ColumnPositionMappingStrategy<Stock> setMapStrategy() {
+	ColumnPositionMappingStrategy<Stock> strategy = new ColumnPositionMappingStrategy<>();
+	strategy.setType(Stock.class);
+	String[] columns = { "Date", "Open", "High", "Low", "Close" };
+	strategy.setColumnMapping(columns);
+	return strategy;
     }
 }
